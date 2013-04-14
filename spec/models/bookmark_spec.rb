8891345e6e0bd17512362d64ed810a08ab54f235
Angela Francis <angela.marie.francis@gmail.com>
2013-04-14 10:47:01 -0500

    Make a basic User model, including passwords

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,5 +1,16 @@
+# == Schema Information
+#
+# Table name: bookmarks
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Bookmark do
-  pending "add some examples to (or delete) #{__FILE__}"
+
 end
