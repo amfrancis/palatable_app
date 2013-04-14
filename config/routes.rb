a549da908473753affeee3e383e5f76f19124c64
Angela Francis <angela.marie.francis@gmail.com>
2013-04-14 09:34:01 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 PalatableApp::Application.routes.draw do
-  get "static_pages/home"
+    get "users/new"
 
-  get "static_pages/help"
+    root to: 'static_pages#home'
 
-  resources :bookmarks
+    match '/help',      to: 'static_pages#help'
+    match '/about',     to: 'static_pages#about'
+    match 'contact',    to: 'static_pages#contact'
+    match '/signup',    to: 'users#new'
 
-  resources :users
+    resources :bookmarks
+    resources :users
 
 end
